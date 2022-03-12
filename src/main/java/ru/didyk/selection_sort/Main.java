ef1426628227e395959ec0c963ddd68e51dfd033
Aleksey Didyk <alekseydidyk@yandex.ru>
2022-03-12 23:09:37 +0300

    add selection sort

---
+++ src/main/java/ru/didyk/selection_sort/Main.java
@@ -0,0 +1,23 @@
+package ru.didyk.selection_sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = {4, 2, 6, 1, 0, 9};
+
+        for (int i = 0; i < numbers.length; i++) {
+            int val = i;
+            for (int j = i; j < numbers.length; j++) {
+                if(numbers[j] < numbers[val]) {
+                    val = j;
+                }
+            }
+            int temp = numbers[i];
+            numbers[i] = numbers[val];
+            numbers[val] = temp;
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+}
