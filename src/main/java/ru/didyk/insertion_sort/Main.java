5fe483f5383db108537738e8deff84940ccc3085
Aleksey Didyk <alekseydidyk@yandex.ru>
2022-03-12 22:13:10 +0300

    add insertion sort

---
+++ src/main/java/ru/didyk/insertion_sort/Main.java
@@ -0,0 +1,21 @@
+package ru.didyk.insertion_sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = {6, 3, 1, 7, 8, 2, 0};
+
+        for (int i = 0; i < numbers.length; i++) {
+            int current = numbers[i];
+            int prev = i - 1;
+            while(prev >= 0 && current < numbers[prev]) {
+                numbers[prev + 1] = numbers[prev];
+                prev--;
+            }
+            numbers[prev + 1] = current;
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+}
