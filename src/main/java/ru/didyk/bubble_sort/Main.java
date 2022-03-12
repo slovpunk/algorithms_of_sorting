7e3263a6d226de3a0968148184fa2f289604bb4e
Aleksey Didyk <alekseydidyk@yandex.ru>
2022-03-12 21:55:07 +0300

    add bubble sort

--- src/main/java/ru/didyk/bubble_sort/Main.java
+++ src/main/java/ru/didyk/bubble_sort/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int[] numbers = {3, 4, 1, 0, 7, 5};
 
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers.length; j++) {
+            for (int j = 0; j < i; j++) {
                 if(numbers[i] < numbers[j]) {
                     int temp = numbers[i];
                     numbers[i] = numbers[j];
