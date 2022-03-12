1291562a35ca690b20626a4214e19970d7d0882e
Aleksey Didyk <alekseydidyk@yandex.ru>
2022-03-12 21:51:40 +0300

    Initial commit

---
+++ src/main/java/ru/didyk/bubble_sort/Main.java
@@ -0,0 +1,22 @@
+package ru.didyk.bubble_sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = {3, 4, 1, 0, 7, 5};
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if(numbers[i] < numbers[j]) {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
